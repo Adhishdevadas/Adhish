33b842f37e203dfdafafd742fe75b5734dbf3359
Adhishdevadasan <144409643+Adhishdevadas@users.noreply.github.com>
2023-10-19 11:15:59 +0530

    Update challenge2.2 (3).py

--- challenge2.2 (3).py
+++ challenge2.2 (3).py
@@ -39,7 +39,7 @@ class BankAccount:
 
 # Create an instance of the BankAccount class
 account = BankAccount(account_number="123456789",
-                      account_holder_name="Hari Prabu",
+                      account_holder_name="Adhish DG",
                       initial_balance=5000.0)
 # Test deposit and withdrawal functionality
 account.display_balance()
